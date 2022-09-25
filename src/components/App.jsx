702e0ab9d63557fe6a8d603e3b4e2f76e8ac99f2
Artem Makhov <makhovartem23@gmail.com>
2022-09-25 10:38:07 +0300

    add shared-layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./GlobalStyle";
-import { Box } from "./Box";
+// import { Box } from "./Box";
 
+import { SharedLayout } from "./SharedLayout/SharedLayout";
 import Home from "../pages/Home/Home";
 import Movies from "../pages/Movies/Movies";
 import { MovieDetails } from "../pages/MovieDetails/MovieDetails";
-import { Header,Link } from "./App.styled";
 import { Cast } from "./Cast/Cast";
 import { Reviews } from "./Reviews/Reviews";
 
 export const App = () => {
   return (
-    <Box
-      m="0 auto"
-      p="0 16px">
-      <Header>
-        <nav>
-          <Link to="/"end>Home</Link>
-          <Link to="/movies">Movies</Link>
-        </nav>
-</Header>
-
-      <Routes>
-        <Route path="/" element={<Home/>} />
+    <>
+      <GlobalStyle/>
+       <Routes>
+        <Route path="/" element={<SharedLayout />} >
+        <Route index element={<Home/>} />
         <Route path="movies" element={<Movies />} />
         <Route path="movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews/>}/>
         </Route>
-        <Route path="*" element={<div>Not Found</div>}/>
+        <Route path="*" element={<div>Not Found</div>} />
+        </Route>
       </Routes>
-      <GlobalStyle/>
-    </Box>
-  )
+    </>
+      
+  );
 };
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+import { Box } from "components/Box";
+import { Header,Link } from "./SharedLayout.styled";
+
+export const SharedLayout = () => {
+    return (
+            <Box
+      m="0 auto"
+      p="0 16px"
+    >
+<Header>
+        <nav>
+          <Link to="/"end>Home</Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+</Header>
+<Outlet/>
+
+</Box>
+    
+    );
+};
